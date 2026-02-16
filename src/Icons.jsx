ce88d9f41ba91e7638814f80ad92936ab6dd0cd4
Varun Selvakumar <varunselvakumar6@gmail.com>
2026-02-16 15:01:16 +0530

    Update Icons.jsx

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -2,7 +2,7 @@ import './typed.css'
 
 function Icons(){
     const skillChips = [
-       "Html","CSS","Bootstrap","JavaScript","TypeScript","Bootstrap","TailwindCSS","ReactJS","Flutter","NodeJS","ExpressJS" 
+       "Html","CSS","Bootstrap","JavaScript","TypeScript","TailwindCSS","ReactJS","Flutter","NodeJS","ExpressJS" 
     ]
 
     const cloudTools = [
